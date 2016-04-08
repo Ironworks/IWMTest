eaee3581c95226308297d5a0befd19f8e809bf93
Trevor Doodes <tdoodes@ironworksmedialimited.co.uk>
2016-04-08 18:43:03 +0100

    Created IWMNetworkManager

---
+++ IWMTest/NetworkManager/IWMNetworkManager.h
@@ -0,0 +1,13 @@
+//
+//  IWMNetworkManager.h
+//  IWMTest
+//
+//  Created by Trevor Doodes on 08/04/2016.
+//  Copyright © 2016 Trevor Doodes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IWMNetworkManager : NSObject
+
+@end
---
+++ IWMTest/NetworkManager/IWMNetworkManager.m
@@ -0,0 +1,13 @@
+//
+//  IWMNetworkManager.m
+//  IWMTest
+//
+//  Created by Trevor Doodes on 08/04/2016.
+//  Copyright © 2016 Trevor Doodes. All rights reserved.
+//
+
+#import "IWMNetworkManager.h"
+
+@implementation IWMNetworkManager
+
+@end
