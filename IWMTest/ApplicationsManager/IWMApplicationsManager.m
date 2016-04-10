a28f878fc37cdf1f83d0d7118af68c461ceb28be
Trevor Doodes <tdoodes@ironworksmedialimited.co.uk>
2016-04-10 22:46:06 +0100

    Implement retrieve applications from server and build results array.

--- IWMTest/ApplicationsManager/IWMApplicationsManager.m
+++ IWMTest/ApplicationsManager/IWMApplicationsManager.m
@@ -8,6 +8,13 @@
 
 #import "IWMApplicationsManager.h"
 #import "IWMNetworkManager.h"
+#import "IWMApplication.h"
+#import "IWMApplicationBuilder.h"
+
+@interface IWMApplicationsManager ()
+
+@property (nonatomic, strong) NSMutableArray *applicationsArray;
+@end
 
 @implementation IWMApplicationsManager
 
@@ -21,10 +28,52 @@
     return self;
 }
 
+- (NSArray *)appsArrayFromaApplicationsArray:(NSArray *)appArray {
+    
+    IWMApplicationBuilder *applicationBuilder = [[IWMApplicationBuilder alloc] init];
+
+    for (NSDictionary *entry in appArray) {
+        IWMApplication *newApp = [applicationBuilder applicationFromDictionary:entry];
+        [self.applicationsArray addObject:newApp];
+    }
+                                  
+    return [self.applicationsArray copy];
+}
+
 - (void)getApplicationsWithSuccessBlock:(successBlock)successBlock
-                                    failureBlock:(failureBlock)failureBlock
+                                  failureBlock:(failureBlock)failureBlock
 {
     
+    __weak typeof (self)weakSelf = self;
+    [self.networkManager retrieveApplicationsSuccess:^(NSArray *applicationsArray) {
+        
+        IWMApplicationsManager *blockSelf = weakSelf;
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (successBlock) {
+                NSArray *appArray = [blockSelf appsArrayFromaApplicationsArray:applicationsArray];
+                successBlock(appArray);
+            }
+        });
+        
+    } failure:^(NSString *message, NSUInteger statusCode) {
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (failureBlock) {
+                failureBlock(message);
+            }
+        });
+    }];
+}
+
+#pragma mark - Accessor Methods
+- (NSMutableArray *)applicationsArray {
+    
+    if (_applicationsArray == nil) {
+        _applicationsArray = [NSMutableArray array];
+    }
+    
+    return _applicationsArray;
 }
 
 @end
